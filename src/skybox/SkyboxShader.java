073235213c79be1e99bf8e51bbc7017fb011af7b
Thomas Lucien Pierre Heurion <thomas.lucien.pierre.heurion@eisti.fr>
2020-08-10 15:30:37 +0200

    28 - Day / Night

--- src/skybox/SkyboxShader.java
+++ src/skybox/SkyboxShader.java
@@ -4,6 +4,8 @@ import org.lwjgl.util.vector.Matrix4f;
 
 import entities.Camera;
 
+import org.lwjgl.util.vector.Vector3f;
+import renderEngine.DisplayManager;
 import shaders.ShaderProgram;
 import toolbox.Maths;
 
@@ -14,6 +16,12 @@ public class SkyboxShader extends ShaderProgram{
 
     private int location_projectionMatrix;
     private int location_viewMatrix;
+    private int location_fogColour;
+    private int location_cubeMap;
+    private int location_cubeMap2;
+    private int location_blendFactor;
+    private static final float ROTATE_SPEED = 1f;
+    private float rotation = 0;
 
     public SkyboxShader() {
         super(VERTEX_FILE, FRAGMENT_FILE);
@@ -28,13 +36,32 @@ public class SkyboxShader extends ShaderProgram{
         matrix.m30 = 0;
         matrix.m31 = 0;
         matrix.m32 = 0;
+        rotation+= ROTATE_SPEED* DisplayManager.getFrameTimeSeconds();
+        Matrix4f.rotate((float) Math.toRadians(rotation),new Vector3f(0,1,0),matrix,matrix);
         super.loadMatrix(location_viewMatrix, matrix);
+    }
+
+    public void loadFogColour(float r, float g, float b){
+        super.loadVector(location_fogColour,new Vector3f(r,g,b));
+    }
+
+    public void connectTextureUnits(){
+        super.loadInt(location_cubeMap,0);
+        super.loadInt(location_cubeMap2,1);
+    }
+
+    public void loadBlendFactor(float blend){
+        super.loadFloat(location_blendFactor,blend);
     }
 
     @Override
     protected void getAllUniformLocations() {
         location_projectionMatrix = super.getUniformLocation("projectionMatrix");
         location_viewMatrix = super.getUniformLocation("viewMatrix");
+        location_fogColour = super.getUniformLocation("fogColour");
+        location_cubeMap = super.getUniformLocation("cubeMap");
+        location_cubeMap2 = super.getUniformLocation("cubeMap2");
+        location_blendFactor = super.getUniformLocation("blendFactor");
     }
 
     @Override
