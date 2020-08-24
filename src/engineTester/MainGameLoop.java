ea27c683821e53d0813f35d8a27f2e7641dc24c7
Thomas Lucien Pierre Heurion <thomas.lucien.pierre.heurion@eisti.fr>
2020-08-24 18:50:20 +0200

    37 - Procedural Terrain

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -66,7 +66,7 @@ public class MainGameLoop {
         fern.getTexture().setNumberOfRows(2);
 
 
-        Light light = new Light(new Vector3f(0,10000,-7000),new Vector3f(0.4f,0.4f,0.4f));
+        Light light = new Light(new Vector3f(0,10000,-7000),new Vector3f(1,1,1));
         List<Light> lights = new ArrayList<>();
         lights.add(light);
 //        lights.add(new Light(new Vector3f(185,10,-293),new Vector3f(2,0,0),new Vector3f(1,0.01f,0.002f)));
